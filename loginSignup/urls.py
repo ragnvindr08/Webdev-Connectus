134e436e9bc14e8450aaaa80429038f557c224ae
ragnvindr08 <johnstevenimperialcaspe08@gmail.com>
2024-04-27 05:05:31 +0800

    Your commit message

---
+++ loginSignup/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", include(("base.urls", "base"), "base"))
+] + static(settings.STATIC_URL)
